5dfb10d8dadf744c709148ea62cec2102c86b539
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-30 11:18:46 +0100

    ✨ feat: Website under construction

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={''} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello there !</h1>
+        <h2>This portfolio is still under construction, so please be patient.</h2>
+        <p>You will be notified when it's ready. </p>
       </header>
+
     </div>
   );
 }

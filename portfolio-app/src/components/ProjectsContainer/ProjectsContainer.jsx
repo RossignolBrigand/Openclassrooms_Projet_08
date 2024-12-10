da497f326aa711f852e8ef2f58987ba03eada3da
Thomas PINEAU <thomas.pineau93@proton.me>
2024-12-10 16:24:41 +0100

    🔨 chore:  built local projects data to render

--- portfolio-app/src/components/ProjectsContainer/ProjectsContainer.jsx
+++ portfolio-app/src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -1,7 +1,14 @@
 
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import './_projectsContainer.scss';
+
+import ProjectCard from '../ProjectCard/ProjectCard';
+import ProjectModal from '../ProjectModal/ProjectModal';
+
+// The project data holds references for localization which are then used with react i18n to showcase the right lines. This enables use to decouple our data from the standard localization files
 import ProjectsData from '../../data/projects.json';
+
+import './_projectsContainer.scss';
 //------------------------------------------------------------
 
 export default function ProjectsContainer({ children }) {

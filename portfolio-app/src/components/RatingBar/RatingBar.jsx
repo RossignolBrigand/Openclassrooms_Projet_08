e823f897a966a66b8e760ae85551a6497110615e
RossignolBrigand <168014867+RossignolBrigand@users.noreply.github.com>
2024-12-17 18:15:13 +0100

    refactor: font changes to get more modern look

--- portfolio-app/src/components/RatingBar/RatingBar.jsx
+++ portfolio-app/src/components/RatingBar/RatingBar.jsx
@@ -1,21 +1,23 @@
-import { BsXDiamondFill } from 'react-icons/bs';
+import { BsSquareFill } from 'react-icons/bs';
 import './_ratingBar.scss';
 
 const DEFAULT_MAX_COUNT = 5;
+const DEFAULT_ACTIVE_COLOR = 'orange';
 
 export default function RatingBar({ rating, label, count }) {
     return (
         <>
             <div className="rating-bar__wrapper">
                 <div className="rating-bar__label">
-                    <h3>Rating bar label {label}</h3>
+                    <h4>{label}:</h4>
                 </div>
                 <div className="rating-bar__container">
                     {[...Array(count || DEFAULT_MAX_COUNT)].map((_, index) => {
                         const starValue = index + 1;
                         return (
-                            <BsXDiamondFill
-                                className={`star ${starValue <= rating ? 'star-filled' : ''}`}
+                            <BsSquareFill
+                                key={index}
+                                className={`star ${starValue <= rating ? `star-filled__${label || DEFAULT_ACTIVE_COLOR}` : ''}`}
                             />
                         );
                     })}
--- portfolio-app/src/layout/Skills/SkillsSection.jsx
+++ portfolio-app/src/layout/Skills/SkillsSection.jsx
@@ -1,63 +1,85 @@
 import { useTranslation } from 'react-i18next';
 
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
+import RatingBar from '../../components/RatingBar/RatingBar';
 
 import './_skillsSection.scss';
 
 function SkillsSection() {
-
-    const { t } = useTranslation('skillSection')
+    const { t } = useTranslation('skillSection');
 
     return (
-        <section className='skills-section' id='skills-section'>
+        <section className="skills-section" id="skills-section">
             <h2>{t('title')}</h2>
-            <div className='skills-container'>
-                <div className='skills-block' >
-                    <h3 className='skills-block__title'>{t('block-titles.frontend')}</h3>
-                    <ul className='skills-block__content--list'>
-                        <li><ProgressBar label={"HTML5"} rating={80} color={"blue"} /></li>
-                        <li><ProgressBar label={"CSS"} rating={70} color={"orange"} /></li>
-                        <li><ProgressBar label={"JavaScript"} rating={60} color={"yellow"} /></li>
-                        <li><ProgressBar label={"React"} rating={60} color={"teal"} /></li>
-                        <li><ProgressBar label={"Sass"} rating={60} color={"pink"} /></li>
+            <div className="skills-container">
+                <div className="skills-block">
+                    <h3 className="skills-block__title">
+                        {t('block-titles.frontend')}
+                    </h3>
+                    <ul className="skills-block__content--list">
+                        <li>
+                            <RatingBar label={'HTML'} rating={4} />
+                        </li>
+                        <li>
+                            <RatingBar label={'CSS'} rating={4} />
+                        </li>
+                        <li>
+                            <RatingBar label={'Javascript'} rating={4} />
+                        </li>
+                        <li>
+                            <RatingBar label={'React'} rating={4} />
+                        </li>
+                        <li>
+                            <RatingBar label={'Sass'} rating={3} />
+                        </li>
                     </ul>
                 </div>
-                <div className='skills-block'>
-                    <h3 className='skills-block__title'>{t('block-titles.backend')}</h3>
-                    <ul className='skills-block__content--list'>
-                        <li><ProgressBar label={"Node.js"} rating={60} color={"green"} /></li>
-                        <li><ProgressBar label={"Express"} rating={60} color={"teal"} /></li>
-                        <li><ProgressBar label={"Mongo.db"} rating={50} color={"green"} /></li>
+                <div className="skills-block">
+                    <h3 className="skills-block__title">
+                        {t('block-titles.backend')}
+                    </h3>
+                    <ul className="skills-block__content--list">
+                        <li>
+                            <RatingBar label={'Node.js'} rating={3} />
+                        </li>
+                        <li>
+                            <RatingBar label={'Express'} rating={4} />
+                        </li>
+                        <li>
+                            <RatingBar label={'MongoDB'} rating={3} />
+                        </li>
                     </ul>
                 </div>
             </div>
-            <div className='tools-container'>
-                <h3 className='tools-block__title'>{t('block-titles.tools&tech')}</h3>
-                <div className='tools-block'>
-                <div className='tools-block__content'>
-                    <h4>{t('applications.title')}</h4>
-                    <ul className='tools-block__content--list'>
-                        <li>Figma</li>
-                        <li>Git</li>
-                        <li>Notion</li>
-                        <li>Miro</li>
-                        <li>Postman</li>
-                        <li>Swagger</li>
-                    </ul>
-                </div>
-                <div className='tools-block__content'>
-                    <h4>{t('dependencies.title')}</h4>
-                    <ul className='tools-block__content--list'>
-                        <li>JSONWebToken</li>
-                        <li>bCrypt</li>
-                        <li>Multer</li>
-                        <li>Sharp</li>
-                        <li>Gulp</li>
-                    </ul>
-                </div>
+            <div className="tools-container">
+                <h3 className="tools-block__title">
+                    {t('block-titles.tools&tech')}
+                </h3>
+                <div className="tools-block">
+                    <div className="tools-block__content">
+                        <h4>{t('applications.title')}</h4>
+                        <ul className="tools-block__content--list">
+                            <li>Figma</li>
+                            <li>Git</li>
+                            <li>Notion</li>
+                            <li>Miro</li>
+                            <li>Postman</li>
+                            <li>Swagger</li>
+                        </ul>
+                    </div>
+                    <div className="tools-block__content">
+                        <h4>{t('dependencies.title')}</h4>
+                        <ul className="tools-block__content--list">
+                            <li>JSONWebToken</li>
+                            <li>bCrypt</li>
+                            <li>Multer</li>
+                            <li>Sharp</li>
+                            <li>Gulp</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div className='learning-container'>
+            <div className="learning-container">
                 <h4>{t('learning.title')}</h4>
                 <ul>
                     <li>Python</li>
@@ -66,11 +88,9 @@ function SkillsSection() {
                     <li>Debug / Testing</li>
                     <li>E-commerce</li>
                 </ul>
-
             </div>
-        </section >
+        </section>
     );
-
-};
+}
 
 export default SkillsSection;
